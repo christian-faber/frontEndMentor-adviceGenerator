1ebbd02db22908baeeb89ca3848ae499ce36c9b8
Christian <christian.a.faber@gmail.com>
2022-06-30 13:13:43 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+async function getAdvice() {
+  const response = await fetch("https://api.adviceslip.com/advice");
+
+  const data = await response.json();
+  console.log(data.slip);
+  displayData(data.slip);
+  displayId(data.slip);
+
+  function displayData(data) {
+    document.getElementById("advice").innerHTML = data.advice;
+  }
+
+  function displayId(data) {
+    document.getElementById("id").innerHTML = `Advice #${data.id}`;
+  }
+}
